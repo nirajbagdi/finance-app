1cfe111c025df632dcc06d05e8f44c352be12a43
Niraj Bagdi <bagdiniraj22@gmail.com>
2025-06-20 15:49:12 +0530

    budget preview into a implicit component

--- src/features/budgets/components/BudgetPreview.tsx
+++ src/features/budgets/components/BudgetPreview.tsx
@@ -13,25 +13,23 @@ type BudgetPreviewProps = {
     transactions: Transaction[];
 };
 
-const BudgetPreview = ({ budgets, transactions }: BudgetPreviewProps) => {
-    return (
-        <div className="grid md:grid-cols-2 gap-12 md:gap-3 items-center justify-items-center h-[80%]">
-            <div className="lg:ml-15">
-                <BudgetChart budgets={budgets} transactions={transactions} />
-            </div>
-
-            <div className="md:flex md:flex-col gap-x-20 gap-y-5 md:gap-6 grid grid-cols-2">
-                {budgets.map((budget, idx) => (
-                    <ColoredLegend
-                        key={idx}
-                        label={budget.category}
-                        value={formatAmount(budget.value)}
-                        theme={budget.theme || '#000'}
-                    />
-                ))}
-            </div>
+const BudgetPreview = ({ budgets, transactions }: BudgetPreviewProps) => (
+    <div className="grid md:grid-cols-2 gap-12 md:gap-3 items-center justify-items-center h-[80%]">
+        <div className="lg:ml-15">
+            <BudgetChart budgets={budgets} transactions={transactions} />
         </div>
-    );
-};
+
+        <div className="md:flex md:flex-col gap-x-20 gap-y-5 md:gap-6 grid grid-cols-2">
+            {budgets.map((budget, idx) => (
+                <ColoredLegend
+                    key={idx}
+                    label={budget.category}
+                    value={formatAmount(budget.value)}
+                    theme={budget.theme || '#000'}
+                />
+            ))}
+        </div>
+    </div>
+);
 
 export default BudgetPreview;
