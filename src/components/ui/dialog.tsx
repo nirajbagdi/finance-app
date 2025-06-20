5d8a662fafd3ae68295d686f4af5db3390c1f116
Niraj Bagdi <bagdiniraj22@gmail.com>
2025-06-20 16:28:42 +0530

    text left on all dialogs on all screen sizes

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -80,10 +80,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<'div'>) {
     return (
         <div
             data-slot="dialog-header"
-            className={cn(
-                'flex flex-col gap-2 text-center sm:text-left mb-1',
-                className
-            )}
+            className={cn('flex flex-col gap-2 text-left mb-1', className)}
             {...props}
         />
     );
